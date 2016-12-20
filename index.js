b362cfff0adabcccb2c5e4da2177ade2a3bbe5c3
Alex Jacobs <lex.jacobs@gmail.com>
2016-12-19 21:15:24 -0800

    pairs output by season with fractions

--- index.js
+++ index.js
@@ -14,12 +14,29 @@ var dates = [{
 }, {
     title: 'winter 2017',
     timestamp: [2017, 12, 21, 8, 27, 54, 306]
+}, {
+    title: 'spring 2018',
+    timestamp: [2018, 3, 20, 9, 15, 24, 776]
+}, {
+  title: 'summer 2018',
+  timestamp: [2018, 6, 21, 3, 7, 15, 146]
+}, {
+  title: 'autumn 2018',
+  timestamp: [2018, 9, 22, 18, 54, 2, 859]
+}, {
+  title: 'winter 2018',
+  timestamp: [2018, 12, 21, 14, 22, 41, 440]
+}, {
+  title: 'spring 2019',
+  timestamp: [2019, 3, 20, 14, 58, 23, 166]
 }];
 
 console.log(datePairs(dates).map((pair) => {
     return {
-        name: pair[0].title,
-        segments: revealBetween(pair[0].timestamp, pair[1].timestamp)
+        season: pair[0].title,
+        segments: revealBetween(pair[0], pair[1]).map((segment) => {
+            return '' + segment.segmentTitle + segment.segmentTime;
+        })
     }
 }));
 
@@ -33,7 +50,10 @@ function datePairs(dates) {
 
 function revealBetween(season1, season2) {
     return quartersBetween(season1, season2).map((segment) => {
-        return makeMoment(segment);
+        return {
+            segmentTitle: segment.segmentTitle,
+            segmentTime: makeMoment(segment.segmentTime)
+        }
     })
 }
 
@@ -46,16 +66,27 @@ function makeDate(a) {
 
 function makeMoment(a) {
     // months are 0 indexed
-    return moment(a).format('YYYY-MM-DD a hh:mm:ss.SSS ZZ')
+    return moment(a).format('YYYY-MM-DD HH:mm:ss.SSS ZZ')
 }
 
-function quartersBetween(d1, d2) {
-    d1 = makeDate(d1);
-    d2 = makeDate(d2);
+function quartersBetween(season1, season2) {
+    var d1 = makeDate(season1.timestamp);
+    var d2 = makeDate(season2.timestamp);
     var result = [];
     var difference = ((+d2 - +d1) / 4);
+
+    var designation = {
+        0: 'SEASON',
+        1: '16TH',
+        2: 'HALF',
+        3: '16TH'
+    }
+
     for (var i = 0; i < 4; i++) {
-        result.push(+d1 + (i * difference));
+        result.push({
+            segmentTitle: season1.title + ' + ' + i + '/16ths. ' + designation[i] + ' ',
+            segmentTime: +d1 + (i * difference)
+        });
     }
     return result;
 }
