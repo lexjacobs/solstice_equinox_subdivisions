530110fbb91f6744fe608b730760c42f5271a60e
Alex Jacobs <lex.jacobs@gmail.com>
2016-12-19 22:35:41 -0800

    Increase range of seasonal inputs.

--- index.js
+++ index.js
@@ -1,35 +1,62 @@
 var moment = require('moment');
-var dates = [{
-    title: 'winter 2016',
-    timestamp: [2016, 12, 21, 3, 44, 9, 851]
-}, {
-    title: 'spring 2017',
-    timestamp: [2017, 3, 20, 3, 28, 37, 4]
-}, {
-    title: 'summer 2017',
-    timestamp: [2017, 6, 20, 21, 24, 7, 853]
-}, {
-    title: 'autumn 2017',
-    timestamp: [2017, 9, 22, 13, 1, 45, 345]
-}, {
-    title: 'winter 2017',
-    timestamp: [2017, 12, 21, 8, 27, 54, 306]
-}, {
-    title: 'spring 2018',
-    timestamp: [2018, 3, 20, 9, 15, 24, 776]
-}, {
-  title: 'summer 2018',
-  timestamp: [2018, 6, 21, 3, 7, 15, 146]
-}, {
-  title: 'autumn 2018',
-  timestamp: [2018, 9, 22, 18, 54, 2, 859]
-}, {
-  title: 'winter 2018',
-  timestamp: [2018, 12, 21, 14, 22, 41, 440]
-}, {
-  title: 'spring 2019',
-  timestamp: [2019, 3, 20, 14, 58, 23, 166]
-}];
+var dates = [
+
+// {title: 'Spring 2019', timestamp: [2019, 03, 20 PM 02:58:23.166]},
+{title: "Spring", timestamp: [2015, 3, 20, 15, 45, 8, 574]},
+{title: "Summer", timestamp: [2015, 6, 21, 9, 37, 55, 37]},
+{title: "Autumn", timestamp: [2015, 9, 23, 1, 20, 31, 812]},
+{title: "Winter", timestamp: [2015, 12, 21, 20, 47, 56, 83]},
+
+{title: "Spring", timestamp: [2016, 3, 19, 21, 30, 10, 630]},
+{title: "Summer", timestamp: [2016, 6, 20, 15, 34, 10, 430]},
+{title: "Autumn", timestamp: [2016, 9, 22, 7, 21, 6, 507]},
+{title: "Winter", timestamp: [2016, 12, 21, 2, 44, 9, 851]},
+
+{title: "Spring", timestamp: [2017, 3, 20, 3, 28, 37, 4]},
+{title: "Summer", timestamp: [2017, 6, 20, 21, 24, 7, 853]},
+{title: "Autumn", timestamp: [2017, 9, 22, 13, 1, 45, 345]},
+{title: "Winter", timestamp: [2017, 12, 21, 8, 27, 54, 306]},
+
+{title: "Spring", timestamp: [2018, 3, 20, 9, 15, 24, 776]},
+{title: "Summer", timestamp: [2018, 6, 21, 3, 7, 15, 146]},
+{title: "Autumn", timestamp: [2018, 9, 22, 18, 54, 2, 859]},
+{title: "Winter", timestamp: [2018, 12, 21, 14, 22, 41, 440]},
+
+{title: "Spring", timestamp: [2019, 3, 20, 14, 58, 23, 166]},
+{title: "Summer", timestamp: [2019, 6, 21, 8, 54, 11, 851]},
+{title: "Autumn", timestamp: [2019, 9, 23, 12, 50, 6, 775]},
+{title: "Winter", timestamp: [2019, 12, 21, 20, 19, 22, 446]},
+
+{title: "Spring", timestamp: [2020, 3, 19, 20, 49, 33, 410]},
+{title: "Summer", timestamp: [2020, 6, 20, 14, 43, 36, 747]},
+{title: "Autumn", timestamp: [2020, 9, 22, 6, 30, 34, 987]},
+{title: "Winter", timestamp: [2020, 12, 21, 2, 2, 16, 268]},
+
+{title: "Spring", timestamp: [2021, 3, 20, 2, 37, 24, 294]},
+{title: "Summer", timestamp: [2021, 6, 20, 20, 32, 5, 495]},
+{title: "Autumn", timestamp: [2021, 9, 22, 12, 21, 0, 855]},
+{title: "Winter", timestamp: [2021, 12, 21, 7, 59, 13, 863]},
+
+{title: "Spring", timestamp: [2022, 3, 20, 8, 33, 20, 419]},
+{title: "Summer", timestamp: [2022, 6, 21, 2, 13, 46, 75]},
+{title: "Autumn", timestamp: [2022, 9, 22, 18, 3, 36, 646]},
+{title: "Winter", timestamp: [2022, 12, 21, 13, 48, 7, 546]},
+
+{title: "Spring", timestamp: [2023, 3, 20, 14, 24, 20, 932]},
+{title: "Summer", timestamp: [2023, 6, 21, 7, 57, 43, 963]},
+{title: "Autumn", timestamp: [2023, 9, 22, 23, 49, 53, 546]},
+{title: "Winter", timestamp: [2023, 12, 21, 19, 27, 16, 131]},
+
+{title: "Spring", timestamp: [2024, 3, 19, 20, 6, 18, 269]},
+{title: "Summer", timestamp: [2024, 6, 20, 13, 50, 53, 786]},
+{title: "Autumn", timestamp: [2024, 9, 22, 5, 43, 33, 103]},
+{title: "Winter", timestamp: [2024, 12, 21, 1, 20, 27, 746]},
+
+{title: "Spring", timestamp: [2025, 3, 20, 2, 1, 22, 434]},
+{title: "Summer", timestamp: [2025, 6, 20, 19, 42, 8, 658]},
+{title: "Autumn", timestamp: [2025, 9, 22, 11, 19, 13, 99]},
+{title: "Winter", timestamp: [2025, 12, 21, 7, 2, 57, 795]}
+];
 
 console.log(datePairs(dates).map((pair) => {
     return {
