99946626ac12e56dd553ec84e6ed012be29ca24c
Alex Jacobs <lex.jacobs@gmail.com>
2016-12-23 12:00:19 -0800

    Cleanup

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 var express = require('express');
 var app = express();
+
 app.set('port', (process.env.PORT || 3000));
+
 app.use(express.static(__dirname + '/public'));
-
-// app.get('/', function(req, res){
-//   res.render('index', {
-//     title: 'Home'
-//   });
-// });
-
 
 app.listen(app.get('port'));
