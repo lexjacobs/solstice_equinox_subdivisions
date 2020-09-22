f228264e89ded8f8600a360f13834c085ff7ca15
Alex Jacobs <lex.jacobs@gmail.com>
2020-09-22 11:42:01 -0700

    Updates

---
+++ precache-manifest.19c8297f262b7ee8ab95a22bb453e56c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce8e46ac78e5e6bd21947d7983061b9e",
+    "url": "/solstice_equinox_subdivisions/index.html"
+  },
+  {
+    "revision": "7cac413df012037a2ed0",
+    "url": "/solstice_equinox_subdivisions/static/css/main.2c5bc0f5.chunk.css"
+  },
+  {
+    "revision": "bda8db169890cfa3349f",
+    "url": "/solstice_equinox_subdivisions/static/js/2.a3027119.chunk.js"
+  },
+  {
+    "revision": "607f4dba556022527177500adaa9aa68",
+    "url": "/solstice_equinox_subdivisions/static/js/2.a3027119.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cac413df012037a2ed0",
+    "url": "/solstice_equinox_subdivisions/static/js/main.a8133487.chunk.js"
+  },
+  {
+    "revision": "2f486f79f8f2e14973b6",
+    "url": "/solstice_equinox_subdivisions/static/js/runtime-main.ebaf4eaf.js"
+  }
+]);
