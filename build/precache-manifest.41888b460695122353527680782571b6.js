511c1d566978b5e95e104e2f71f94df335faf70a
Alex Jacobs <lex.jacobs@gmail.com>
2020-09-22 11:36:14 -0700

    Add build

---
+++ build/precache-manifest.41888b460695122353527680782571b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d86865e22beaae69bb2aaa37faa54bc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93388ddddad230bddd1e",
+    "url": "/static/css/main.2c5bc0f5.chunk.css"
+  },
+  {
+    "revision": "050ea8b74ad57b15bbe8",
+    "url": "/static/js/2.4f961ed6.chunk.js"
+  },
+  {
+    "revision": "607f4dba556022527177500adaa9aa68",
+    "url": "/static/js/2.4f961ed6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93388ddddad230bddd1e",
+    "url": "/static/js/main.aed8c77d.chunk.js"
+  },
+  {
+    "revision": "27e2bd5de7b14f81db51",
+    "url": "/static/js/runtime-main.1e6ae293.js"
+  }
+]);
